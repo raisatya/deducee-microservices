71d49674438324c82db626dae883f9266c1e319c
raisatya <satyanlpid1@gmail.com>
2024-05-18 09:39:25 +0530

    Fixed connection url issue

--- classroom/src/index.ts
+++ classroom/src/index.ts
@@ -1,26 +1,24 @@
 import { app } from './app';
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
 
-const CONNECTION_URL = "mongodb+srv://raisatya23:atleast10charswith%2A%40SYMBOLS%2DdeduceeCluster0@deduceecluster0.zedj4h4.mongodb.net/?retryWrites=true&w=majority"
+dotenv.config();
+
 
 const start = async () => {
-    /*
-      if (!process.env.JWT_KEY) {
-        throw new Error('[Auth] JWT_KEY must be defined');
-      }
-      
-      if (!process.env.MONGO_URI) {
-        throw new Error('[Auth] MONGO_URI must be defined');
-      }
-    */
   try {
-    await mongoose.connect(CONNECTION_URL);
-    console.log('[Auth] Connected to MongoDB');
+
+    if (!process.env.CONNECTION_URL) {
+      throw new Error("[Classroom] MONGO_URI must be defined");
+    }
+
+    await mongoose.connect(process.env.CONNECTION_URL);
+    console.log('[Classroom] Connected to MongoDB');
   } catch (err) {
     console.log(err);
   }
   app.listen(5000, () => {
-    console.log('[Auth] Listening on port 5000');
+    console.log('[Classroom] Listening on port 5000');
   });
 };
 
